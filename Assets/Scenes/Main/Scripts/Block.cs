fca7916413d86da8e9d8e684e9b4c1a572cdcfa5
happou31 <happo31@gmail.com>
2019-08-15 22:51:35 +0900

    first commit ブロック入れ替えできるようになった

---
+++ Assets/Scenes/Main/Scripts/Block.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+     public enum Type
+    {
+        NONE,
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW
+    }
+
+    public Type type;
+
+    public Material blueMaterial;
+    public Material redMaterial;
+    public Material yellowMaterial;
+    public Material greenMaterial;
+
+    public bool isSelected = false; 
+
+    private new Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (type)
+        {
+            case Type.NONE:
+                renderer.material.color = new Color(0, 0, 0, 0.0f);
+                break;
+            case Type.RED:
+                renderer.material = this.redMaterial;
+                break;
+            case Type.BLUE:
+                renderer.material = this.blueMaterial;
+                break;
+            case Type.GREEN:
+                renderer.material = this.greenMaterial;
+                break;
+            case Type.YELLOW:
+                renderer.material = this.yellowMaterial;
+                break;
+        }
+
+        if (type != Type.NONE)
+        {
+            if (isSelected)
+            {
+                renderer.material.color = new Color(1.0f, 0.5f, 0.5f, 1);
+            }
+            else
+            {
+                renderer.material.color = new Color(1, 1, 1, 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Main/Scripts/Field.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static Block;
+
+public class Field : MonoBehaviour
+{
+
+    public int Width;
+
+    public int Height;
+
+    int cursorX = 0;
+    int cursorY = 0;
+
+    public Block blockPrefab;
+
+    List<Block> blocks = new List<Block>();
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        var r = new System.Random();
+
+        cursorX = Width / 2;
+        cursorY = Height / 2;
+
+        for(var i = 0; i < Width * Height; ++i)
+        {
+            var block = Instantiate(blockPrefab);
+            block.type = (Type)r.Next(5);
+            block.transform.position = new Vector3(i % Width - Width / 2, i / Height);
+            blocks.Add(block);
+        }
+        UpdateCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (var y = Height - 1; y >= 0; --y)
+        {
+            for (var x = 0; x < Width; ++x)
+            {
+                var block = blocks[y * Width + x];
+                block.isSelected = false;
+                // そのマスが空白だったら上に乗っているブロックを一つずつ下にずらす
+                if (block.type == Type.NONE)
+                {
+                    var by = y;
+                    for (var cy = y + 1; cy < Height; ++cy )
+                    {
+                        blocks[by * Width + x].type = blocks[cy * Width + x].type;
+                        by = cy;
+                    }
+
+                    blocks[by * Width + x].type = Type.NONE;
+                }
+            }
+        }
+
+        var XAxis = Input.GetAxis("Horizontal");
+        var YAxis = Input.GetAxis("Vertical");
+
+        if (XAxis > 0 && cursorX < Width - 2)
+        {
+            cursorX++;
+        }
+        if (XAxis < 0 && cursorX > 0)
+        {
+            cursorX--;
+        }
+        if (YAxis > 0 && cursorY < Height - 1)
+        {
+            cursorY++;
+        }
+        if (YAxis < 0 && cursorY > 0)
+        {
+            cursorY--;
+        }
+
+        if (Input.GetButtonDown("BlockChange"))
+        {
+            var t = blocks[cursorY * Width + cursorX].type;
+            blocks[cursorY * Width + cursorX].type = blocks[cursorY * Width + cursorX + 1].type;
+            blocks[cursorY * Width + cursorX + 1].type = t;
+        }
+
+        UpdateCursor();
+
+        Debug.Log("cursorX:" + cursorX);
+        Debug.Log("cursorY:" + cursorY);
+        Debug.Log("XAxis:" + XAxis);
+        Debug.Log("YAxis:" + YAxis);
+    }
+
+    void UpdateCursor()
+    {
+        blocks[cursorY * Width + cursorX].isSelected = true;
+        blocks[cursorY * Width + cursorX + 1].isSelected = true;
+    }
+}
